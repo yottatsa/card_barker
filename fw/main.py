c37bfadf0312daadbaa4f220743b316c35e1799d
Atsuko Ito <me@yottatsa.name>
2021-12-04 22:48:59 +0000

    full eeprom

--- fw/main.py
+++ fw/main.py
@@ -1,5 +1,6 @@
 from enum import Flag, auto
 
+
 class ICR0(Flag):
     """
     00h Interface Configuration Register 0
@@ -17,6 +18,7 @@ class ICR0(Flag):
     CLOCK_IN = CLOCK0 | CLOCK1
     FORCE_PCMCIA = EN_OVERIDE3
 
+
 class IER(Flag):
     """
     01h Interrupt Enable Register
@@ -30,6 +32,7 @@ class IER(Flag):
     CCR0_OVERIDE = auto()
     EN_INPACK = auto()
     UNSET = 0
+
 
 class ICR1(Flag):
     """
@@ -46,10 +49,12 @@ class ICR1(Flag):
     EN_DASP_EXT = auto()
     UNSET = 0
 
+
 class ICR2(Flag):
     """
     03h Interface Configuration Register 2
     """
+
     EN_MEM_MODE = auto()
     EN_INDP_MODE = auto()
     EN_ATT_MODE = auto()
@@ -57,11 +62,14 @@ class ICR2(Flag):
     EN_INVERT_HCS1 = auto()
     EN_INVERT_ATRST = auto()
     EN_IO_MODE = auto()
+    PCMCIA_IO8 = EN_INDP_MODE | EN_ATT_MODE | EN_IO_MODE
+
 
 class ICR3(Flag):
     """
     04h Interface Configuration Register 3
     """
+
     SEL_PRIMARY_1x = auto()
     SEL_PRIMARY_3x = auto()
     SEL_SECOND_1x = auto()
@@ -72,10 +80,12 @@ class ICR3(Flag):
     EN_PDIAG_EXT = auto()
     UNSET = 0
 
+
 class CCRBaseAddress(Flag):
     """
     05h BCMCIA CCR Base Address Register
     """
+
     EN_CRR_A4 = auto()
     EN_CRR_A5 = auto()
     EN_CRR_A6 = auto()
@@ -87,35 +97,13 @@ class CCRBaseAddress(Flag):
     NONE = DIS_CRR_MODE
     CCR_BASE_0000 = 0
 
-class ISR(Flag):
-    """
-    06h PCMCIA Interrupt Status Register
-    """
-    EEPROM = 0
-
-class ESR(Flag):
-    """
-    07h PCMCIA Exception Status Register
-    """
-    EEPROM = 0
-
-class ARA(Flag):
-    """
-    08h Attribute RAM Address Register
-    """
-    EEPROM = 0
-
-class ARD(Flag):
-    """
-    09h Attribute RAM Data Register
-    """
-    EEPROM = 0
 
 class CCR0(Flag):
     """
     0Ah PCMCIA Configuration Option Register
     XX0h
     """
+
     B0 = auto()
     B1 = auto()
     B2 = auto()
@@ -125,26 +113,32 @@ class CCR0(Flag):
     LEVEL_REQUEST = auto()
     SRESET = auto()
     UNSET = 0
+    PCMCIA_IO8 = B5
+
 
 class CCR1(Flag):
     """
     0Bh Card Configuration and Status Register
     XX2h
     """
+
     _R0 = auto()
-    INTERRUPT = auto()
+    INTERRUPT_STATE = auto()
     POWER_DOWN = auto()
     AUDIO = auto()
     _R4 = auto()
     IOIS8 = auto()
     SIGCHG = auto()
     _C7 = auto()
+    PCMCIA_IO8 = IOIS8
+
 
 class CCR2(Flag):
     """
     0Ch Pin Replacement Register
     XX4h
     """
+
     RWPROT = auto()
     RRDY_BSY = auto()
     RBVD2 = auto()
@@ -155,35 +149,193 @@ class CCR2(Flag):
     CBVD1 = auto()
     UNSET = 0
 
+
 class CCR3(Flag):
     """
     0Dh Socket and Copy
     XX6h
     """
+
     UNSET = 0
 
-fw = [
-    ICR0.CLOCK_IN | ICR0.FORCE_PCMCIA | ICR0.EN_RDY_BSY,
-    IER.UNSET,
-    ICR1.UNSET,
-    ICR2.EN_INDP_MODE | ICR2.EN_ATT_MODE,
-    ICR3.UNSET,
-    CCRBaseAddress.CCR_BASE_0000,
-    ISR.EEPROM,
-    ESR.EEPROM,
-    ARA.EEPROM,
-    ARD.EEPROM,
-    CCR0.UNSET,
-    CCR1.IOIS8,
-    CCR2.UNSET,
-    CCR3.UNSET,
-    0,
-    0,
 
-    
+class W1CR(Flag):
+    DIS_PAC1 = auto()
+    UNSET = 0
+    DISABLED = DIS_PAC1
 
 
+class W1SAL(Flag):
+    UNSET = 0
 
-]
 
-print(fw)
+class W1SRAM(Flag):
+    UNSET = 0
+
+
+class W1RAL(Flag):
+    UNSET = 0
+
+
+class W2CR(Flag):
+    DIS_PAC2 = auto()
+    UNSET = 0
+    DISABLED = DIS_PAC2
+
+
+class W2SAL(Flag):
+    UNSET = 0
+
+
+class W2SRAM(Flag):
+    UNSET = 0
+
+
+class W2RAL(Flag):
+    UNSET = 0
+
+
+class W3CR(Flag):
+    DIS_PAC3 = auto()
+    UNSET = 0
+    DISABLED = DIS_PAC3
+
+
+class W3SAL(Flag):
+    UNSET = 0
+
+
+class W3SRAM(Flag):
+    UNSET = 0
+
+
+class W3RAL(Flag):
+    UNSET = 0
+
+
+class EEPROMValidFlag(Flag):
+    UNSET = 0
+
+
+class DDCR(Flag):
+    UNSET = 0
+
+
+PM_TIMER_VAL = 0
+
+
+class PMCR(Flag):
+    EN_8BIT_MODE = auto()
+    EN_MODEM_ALT = auto()
+    EN_CLK = auto()
+    EN_PADS1 = auto()
+    EN_TIMER = auto()
+    EN_PM_RDY = auto()
+    EN_EXT_PD = auto()
+    EN_EXPD_POL = auto()
+    UNSET = 0
+    PCMCIA_IO8 = EN_8BIT_MODE
+
+
+class ICR4(Flag):
+    UNSET = 0
+
+
+class CICR1(Flag):
+    UNSET = 0
+
+
+class CICR2(Flag):
+    MEM_INDX0 = auto()
+    MEM_INDX1 = auto()
+    MEM_INDX2 = auto()
+    EN_MEM_INDX = auto()
+    IO_INDP_INDX4 = auto()
+    IO_INDP_INDX5 = auto()
+    IO_INDP_INDX6 = auto()
+    EN_IO_INDP_INDX = auto()
+    PCMCIA_IO8 = EN_IO_INDP_INDX
+    UNSET = 0
+
+
+class BCR2(Flag):
+    EN_BHE_POL = auto()
+    EN_16_DUECE = auto()
+    EN_DIV_ADDR = auto()
+    EN_MAP_IO_MEM = auto()
+    DUECE_WIDTH4 = auto()
+    DUECE_WIDTH5 = auto()
+    DUECE_ACCESS_DLY6 = auto()
+    DUECE_ACCESS_DLY7 = auto()
+    UNSET = 0
+    PCMCIA_IO8 = 0  # fixme
+
+
+def gen_config(revision_control=0, pm_timer=PM_TIMER_VAL):
+    EEPROM = 0
+    RESERVED = 0
+    Z16017_ONLY = 0
+    BA_ONLY = 0
+    EEPROM_VALID = 0x1C
+    return [
+        ICR0.CLOCK_IN | ICR0.FORCE_PCMCIA | ICR0.EN_RDY_BSY,
+        IER.UNSET,
+        ICR1.UNSET,
+        ICR2.PCMCIA_IO8,
+        ICR3.UNSET,
+        CCRBaseAddress.CCR_BASE_0000,
+        EEPROM,
+        EEPROM,
+        EEPROM,
+        EEPROM,
+        CCR0.UNSET,  # Configuration Option
+        CCR1.PCMCIA_IO8,  # Card Configuration and Status
+        CCR2.UNSET,
+        CCR3.UNSET,
+        RESERVED,
+        RESERVED,
+        W1CR.DISABLED,
+        W1SAL.UNSET,
+        W1SRAM.UNSET,
+        W1RAL.UNSET,
+        W2CR.DISABLED,
+        W2SAL.UNSET,
+        W2SRAM.UNSET,
+        W2RAL.UNSET,
+        W3CR.DISABLED,
+        W3SAL.UNSET,
+        W3SRAM.UNSET,
+        W3RAL.UNSET,
+        RESERVED,
+        RESERVED,
+        EEPROM_VALID,
+        BA_ONLY,
+        EEPROM,
+        EEPROM,
+        EEPROM,
+        revision_control,  # Revision Control
+        0,  # Revision Number,
+        RESERVED,
+        Z16017_ONLY,
+        Z16017_ONLY,
+        DDCR.UNSET,
+        RESERVED,
+        pm_timer,
+        PMCR.PCMCIA_IO8,
+        ICR4.UNSET,
+        CICR1.UNSET,
+        CICR2.PCMCIA_IO8,
+        BCR2.PCMCIA_IO8,
+    ]
+
+
+if __name__ == "__main__":
+    config = gen_config()
+    print(config)
+    with open("fw.bin", "wb+") as f:
+        for val in config + [0] * 208:
+            if isinstance(val, Flag):
+                b = val.value
+            else:
+                b = val
+            f.write(bytes([b]))
